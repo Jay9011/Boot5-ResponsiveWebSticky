3716a88b90d4c547a0412fad8a420355cbb436ad
Jay9011 <whdtjq5904@gmail.com>
2021-10-20 17:06:31 +0900

    RSA 테스트

--- dist/js/RSA.js
+++ dist/js/RSA.js
@@ -2,4 +2,5 @@
 
 function RsaEncrypt(text) {
   var pubKey = "<RSAKeyValue><Modulus>9UQ9soAeUu1LYVjSGXTshxf3apuR7oueEro3OMIy4/c00SQROBil+d0u0FaSeGEa7xc4Xj6OY7RfGycg6rBBHjnToN9Ezfr3yExbF5j3Ny0jPTXiOs9chcRzJN01tL5rImaJOExNcYMCNM6UySg6DNuKWMPv2waoMhQhwuX7X1k=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+  var rsa = new System.Security.Cryptograp();
 }
